8b2b692156c21d7c8aa3ade00d0911044d9260f9
dreamingrainbow <michaeladennis@yahoo.com>
2018-03-05 12:06:08 -0700

    Update fizzbuzz.c

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,7 +9,21 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for(int i = 0; i< n; i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            printf("fizzbuzz");
+        } else if(i % 3 == 0) {
+            printf("fizz");
+        } else if (i % 5 == 0) {
+            printf("buzz");   
+        } else {
+            printf("\n");
+            counter++;
+        }
+        
+    }
+    return counter;
 }
 
 #ifndef TESTING
